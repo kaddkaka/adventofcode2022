1e4b8eb63045d54eab8297a38bf167b7309bd2fd
kaddkaka <kaddkaka@gmail.com>
2022-12-20 22:13:05 +0100

    day6: make a set directly from string slice

--- day6/day6.nim
+++ day6/day6.nim
@@ -1,16 +1,12 @@
-import std/enumerate
 import std/setutils
 
 
 proc how_many(n: int): int =
-  var prev: seq[char]
-  prev.setLen(n)
-
-  for line in lines "input":
-    for i, char in enumerate line:
-      prev[i mod n] = char
-      if i > 3 and toSet(prev).len == n:
-        return i + 1
+  let f = open("input")
+  let line = f.readline()
+  for i in 0..line.high:
+    if toSet(line[i..<i+n]).len == n:
+        return i + n - 1
 
 echo how_many(4)
 echo how_many(14)
